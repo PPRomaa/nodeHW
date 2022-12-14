29216bd5c27567f202cc2e45271b93ff3a26a12b
PPRomaa <popadinets.roman1@gmail.com>
2022-12-14 16:26:55 +0200

    lesson3

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -1,19 +1,61 @@
-const userDb = require("../database/users");
 const ApiError = require("../error/ApiError");
+const {fileService} = require("../service");
 
 module.exports = {
-    checkIsUserExist: (req, res, next) => {
+    checkIsUserExist: async (req, res, next) => {
         try {
             const {userId} = req.params;
 
-            const user = userDb[userId];
+            const users = await fileService.reader()
 
-            if (!user){
-                throw new ApiError('User not found',404);
+            const user = users.find((u) => u.id === +userId);
+            if (!user) {
+                throw new ApiError('User not found', 404);
+            }
+
+            req.users = users;
+            req.user = user;
+            next();
+        } catch (e) {
+            next(e);
+        }
+    },
+    isBodyValidCreate: (req, res, next) => {
+        try {
+            const {name, age} = req.body;
+            if (!name || name.length < 3 || typeof name !== "string") {
+                throw new ApiError('Wrong name', 400);
+            }
+            if (!age ||age < 0 || Number.isNaN(+age)) {
+                throw new ApiError('Wrong age', 400);
             }
             next();
-            req.user = user;
-        }catch (e){
+        } catch (e) {
+            next(e);
+        }
+    },
+    isBodyValidUpdate: (req, res, next) => {
+        try {
+            const {name, age} = req.body;
+            if (name && (name.length < 3 || typeof name !== "string")) {
+                throw new ApiError('Wrong name', 400);
+            }
+            if (age && (age < 0 || Number.isNaN(+age))) {
+                throw new ApiError('Wrong age', 400);
+            }
+            next();
+        } catch (e) {
+            next(e);
+        }
+    },
+    isIdValid: (req, res, next) => {
+        try {
+            const {userId} = req.params;
+            if (userId < 0 || Number.isNaN(+userId)) {
+                throw new ApiError('Not valid id', 400);
+            }
+            next();
+        } catch (e) {
             next(e);
         }
     }
